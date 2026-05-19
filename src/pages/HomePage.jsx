cd36f2f8d687b406c6dc8b12b1d167d2d9eca671
Shashikr36 <137677581+Shashikr36@users.noreply.github.com>
2026-05-19 23:19:55 +0530

    Update API base URL in HomePage component

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -39,7 +39,7 @@ const HomePage = () => {
   };
 
   const api = axios.create({
-    baseURL: "https://expensify-expense-backend.onrender.com/api/v1", // Specify the base URL for your API
+    baseURL: "https://expensify-expense-backend-1.onrender.com/api/v1", // Specify the base URL for your API
   });
 
   //table data
